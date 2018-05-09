f157387d634627e32d13466166c82f8e6a74e99f
Caio Ribeiro <jesser01@outlook.com>
2018-05-09 20:39:13 -0300

    inicio da pagina de cliente

--- client/src/components/clients/scripts.js
+++ client/src/components/clients/scripts.js
@@ -1,3 +1,64 @@
+import axios from 'axios'
+
 export default {
-  name: 'Clients'
+  data () {
+    return {
+      totalItems: null,
+      products: [],
+      items: [],
+      search: '',
+      headers: [{
+        text: 'Descrição do Produto',
+        align: 'left',
+        value: 'nome'
+      },
+      {
+        text: 'Cod. Barras',
+        value: 'codigoBarra'
+      },
+      {
+        text: 'Estoque',
+        value: 'estoque'
+      },
+      {
+        text: 'Preço Compra',
+        value: 'precoCompra'
+      },
+      {
+        text: 'Preço Venda',
+        value: 'precoVenda'
+      },
+      {
+        text: 'Desconto',
+        value: 'desconto'
+      }]
+    }
+  },
+  created () {
+    axios.get(`http://localhost:58538/api/clients`)
+      .then(response => {
+        this.items = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+  },
+  computed: {
+    pages () {
+      if (this.pagination.rowsPerPage == null ||
+        this.pagination.totalItems == null
+      ) return 0
+      return Math.ceil(this.pagination.totalItems / this.pagination.rowsPerPage)
+    }
+  },
+  methods: {
+    ola () {
+      alert('ola')
+    }
+  }
 }
+
+    
+
+  
+
--- client/src/components/header-bar/index.vue
+++ client/src/components/header-bar/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-
+  <div class="header-content">
+   <div class="photo">foto</div>
   </div>
 </template>
 
 <script src="./scripts.js"></script>
+
+<style src="../../css/header-bar.css"></style>
