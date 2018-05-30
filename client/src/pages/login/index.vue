1047e7f86624b88640855451950b939cbe70e02e
Lucas Bruno <lucasbrunoferreira@gmail.com>
2018-05-30 00:31:37 -0300

    Estruturação da pagina de login

---
+++ client/src/pages/login/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login-page">
+    <section class="login-container">
+      <div class="logo">
+        <img src="/static/assets/images/logo.png">
+      </div>
+      <div class="login-form">
+        <form action="">
+          <div class="login-input">
+            <img src="/static/assets/icons/user.png">
+            <input type="text" placeholder="usuario">
+          </div>
+
+          <div class="login-input">
+            <img src="/static/assets/icons/password.png">
+            <input type="password" placeholder="senha">
+          </div>
+          <button>login</button>
+        </form>
+      </div>
+
+      <div class="bar-colored"></div>
+    </section>
+  </div>
+</template>
+
+<script src="./scripts"></script>
+
+<style lang="scss">
+    @import '../../style/style.scss';
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import { LoginPage } from '../pages'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // }
+    { path: '/', redirect: '/login' },
+    {
+      path: '/login',
+      name: 'LoginPage',
+      component: LoginPage
+    }
   ]
 })
